41e19b732d7f6dfc5e8378c2c06b699f50efb5fe
Jakub Horáček <horacek2001@gmail.com>
2024-08-12 16:50:51 +0200

    Add a HUD

--- Assets/Scripts/TrainControl.cs
+++ Assets/Scripts/TrainControl.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
+using TMPro;
 using System.ComponentModel;
 
 public class trainControl : MonoBehaviour
 {
+    [Header("Train Components")]
     private Rigidbody rb;
+    [Header("Train Control")]
     public float maxForwardSpeedKmh = 200.0f; // Max forward speed in km/h
     public float maxBackwardSpeedKmh = 100.0f; // Max backward speed in km/h
     public float forwardAcceleration = 20.0f; // Acceleration rate for forward movement in m/s^2
@@ -14,6 +18,16 @@ public class trainControl : MonoBehaviour
     [SerializeField, ReadOnly(true)]
     private float currentSpeedKmh = 0.0f; // Current speed in km/h
     private bool autopilot = false; // Autopilot mode flag
+
+    [Header("HUD")]
+    public Color forwardColor = Color.blue; // #31FCFE
+    public Color backwardColor = Color.red; // #872727
+    public Image ForwardSpeedometer;
+    public Image BackwardSpeedometer;
+    public TextMeshProUGUI SpeedText;
+    public GameObject AutopilotIndicator;
+
+    [Header("Spawn Manager")]
     public SpawnManager spawnManager;
 
     // Start is called before the first frame update
@@ -74,6 +88,7 @@ public class trainControl : MonoBehaviour
 
         // Convert current speed from m/s to km/h for display and storage
         currentSpeedKmh = MsToKmh(currentSpeedMs);
+        UpdateSpeedometer(currentSpeedKmh);
 
         // Apply the current speed to the train's velocity
         Vector3 velocity = rb.velocity;
@@ -100,14 +115,31 @@ public class trainControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             autopilot = !autopilot;
-            Debug.Log("Autopilot " + (autopilot ? "enabled" : "disabled"));
+            AutopilotIndicator.SetActive(autopilot);
         }
 
         if (autopilot && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)))
         {
             autopilot = false;
-            Debug.Log("Autopilot disabled");
+            AutopilotIndicator.SetActive(false);
         }
+    }
+
+    void UpdateSpeedometer(float speed)
+    {
+        if (speed >= 0)
+        {
+            ForwardSpeedometer.fillAmount = speed / maxForwardSpeedKmh;
+            BackwardSpeedometer.fillAmount = 0;
+        }
+        else
+        {
+            ForwardSpeedometer.fillAmount = 0;
+            BackwardSpeedometer.fillAmount = -speed / maxBackwardSpeedKmh;
+        }
+
+        SpeedText.text = Mathf.Abs(speed).ToString("F0");
+        SpeedText.color = speed >= 0 ? forwardColor : backwardColor;
     }
 
     private void OnTriggerEnter(Collider other)
